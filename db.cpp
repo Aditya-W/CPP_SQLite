f4306a31231e21762aa279fe72314ff20f64ac67
Aditya-W <adityav0810@gmail.com>
2026-05-07 20:44:35 +0530

    Added pages and row serialization

--- db.cpp
+++ db.cpp
@@ -2,9 +2,25 @@
 #include <string>
 #include <sstream>
 #include <cstdint>
+#include <cstring>
 
 const uint32_t COLUMN_USERNAME_SIZE = 32;
 const uint32_t COLUMN_EMAIL_SIZE = 255;
+
+const uint32_t PAGE_SIZE = 4096;
+const uint32_t TABLE_MAX_PAGES = 100;
+
+const uint32_t ID_SIZE = sizeof(uint32_t);
+const uint32_t USERNAME_SIZE = sizeof(char) * (COLUMN_USERNAME_SIZE + 1);
+const uint32_t EMAIL_SIZE = sizeof(char) * (COLUMN_EMAIL_SIZE + 1);
+
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE/ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = TABLE_MAX_PAGES * ROWS_PER_PAGE;
 
 enum class MetaCommandResult
 {
@@ -23,6 +39,12 @@ enum class StatementType
 {
     STATEMENT_INSERT,
     STATEMENT_SELECT
+};
+
+struct Table
+{
+    uint32_t numRows;
+    void* pages[TABLE_MAX_PAGES];
 };
 
 struct Row
@@ -89,6 +111,24 @@ void executeStatement(Statement& statement)
             std::cout << "This is where the select statement executes.\n";
             break;
     }
+}
+
+void serializeRow(Row* source, void* destination)
+{
+    char* dest = static_cast<char*>(destination);
+
+    memcpy(dest + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(dest + USERNAME_OFFSET, source->username, USERNAME_SIZE);
+    memcpy(dest + EMAIL_OFFSET, source->email, EMAIL_SIZE);
+}
+
+void deserializeRow(void* source, Row* destination)
+{
+    char* sour = static_cast<char*>(source);
+
+    memcpy(&(destination->id), sour + ID_OFFSET, ID_SIZE);
+    memcpy(destination->username, sour + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(destination->email, sour + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
 int main()
